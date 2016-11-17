e27e85ef13dad841cc41d7298f34e03781848f1b
Qi Ming <mqi@fraunhofer.sg>
2016-11-17 13:13:47 +0800

    basic reusable component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail.component';
 
 const HEROES: Hero[] = [
   { id: 11, name: 'Mr. Nice' },
@@ -81,14 +79,8 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-          <label>name: </label>{{selectedHero.name}}
-          <input [(ngModel)]="selectedHero.name" placeholder="name">
-      </div>
-    </div>`,
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    `,
 
 })
 
